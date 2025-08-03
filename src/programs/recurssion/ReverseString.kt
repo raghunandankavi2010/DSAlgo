dc97f73ee66d87e2df0ddd63c5cbb248e8f7bf12
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2025-08-03 08:06:48 +0530

    Reverse a string using recursion

--- src/programs/recurssion/ReverseString.kt
+++ src/programs/recurssion/ReverseString.kt
@@ -2,9 +2,13 @@ package programs.recurssion
 
 
 fun main() {
-    val input = "R"
+    val input = "Hello"
+    val s = input.toCharArray()
     val output = reverseString(input)
-    println("Reversed String using recursion $output")
+    println("Reversed String using recursion2 $output")
+    helper(s, 0, input.length - 1)
+    println("Reversed String using recursion2 $output")
+    println(s)
 }
 
 private fun reverseString(input: String): String {
@@ -14,7 +18,22 @@ private fun reverseString(input: String): String {
 
 private fun reverseInputString(input: String, index: Int,  reversed: String): String {
     if(index < 0) return reversed
-    val revString= reversed + input[index]
+    val revString = reversed + input[index]
 
     return reverseInputString(input.take(index),index-1,revString)
+}
+
+
+private fun helper(s: CharArray, left: Int, right: Int) {
+
+    if(left > right) {
+        return
+    }
+
+    val temp = s[left]
+    s[left] = s[right]
+    s[right] = temp
+
+    helper(s,left +1, right -1)
+
 }
