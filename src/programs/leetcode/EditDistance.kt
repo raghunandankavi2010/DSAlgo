29ad053df30789d0303a50558901d3c4f9a9fdb5
Raghunandan K <Raghunandan.K@ril.com>
2025-09-16 11:18:16 +0530

    Edit distance leetcode

---
+++ src/programs/leetcode/EditDistance.kt
@@ -0,0 +1,39 @@
+package programs.leetcode
+
+fun main() {
+    val word1 = "horse"
+    val word2 = "ros"
+    println(minDistance(word1, word2))
+
+}
+
+
+fun minDistance(word1: String, word2: String): Int {
+    val m = word1.length
+    val n = word2.length
+    val dp = Array(m + 1) { IntArray(n + 1) }
+
+    for (i in 0..m) {
+        dp[i][0] = i
+    }
+    for (j in 0..n) {
+        dp[0][j] = j
+    }
+
+    // Fill the DP table
+    for (i in 1..m) {
+        for (j in 1..n) {
+            if (word1[i - 1] == word2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1]
+            } else {
+                dp[i][j] = 1 + minOf(
+                    dp[i - 1][j],
+                    dp[i][j - 1],
+                    dp[i - 1][j - 1]
+                )
+            }
+        }
+    }
+
+    return dp[m][n]
+}
