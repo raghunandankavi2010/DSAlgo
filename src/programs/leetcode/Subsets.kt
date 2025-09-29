99324895e74be3ae416848f10b62b6c2a22d4806
Raghunandan K <Raghunandan.K@ril.com>
2025-09-29 06:32:24 +0530

    Subsets

---
+++ src/programs/leetcode/Subsets.kt
@@ -0,0 +1,44 @@
+package programs.leetcode
+
+
+/**
+ *
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ */
+fun main() {
+    val arr = intArrayOf(1,2,3)
+    println(subsets(arr))
+}
+
+/**
+ * At each step we have 2 choices include current or exclude current
+ * if n is the number of elements for each n we have 2 such choices
+ * So at each step its 2 power n. n time 2 power n is because copying
+ * n elements to result
+ */
+
+fun subsets(nums: IntArray): List<List<Int>> {
+
+    val currentSubset = mutableListOf<Int>()
+    val result = mutableListOf<List<Int>>()
+
+    fun backtrack(index: Int) {
+        result.add(ArrayList(currentSubset))
+        for (i in index until nums.size) {
+            currentSubset.add(nums[i])
+            backtrack(i + 1)
+            currentSubset.removeAt(currentSubset.size - 1)
+        }
+    }
+    backtrack(0)
+    return result
+}
