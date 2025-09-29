35d76e75c2fde6c5e7bf88baf1ff606295f92b3a
Raghunandan K <Raghunandan.K@ril.com>
2025-09-29 11:07:19 +0530

    Subsets with duplicate nums

--- src/programs/leetcode/Subsets.kt
+++ src/programs/leetcode/Subsets.kt
@@ -11,12 +11,13 @@ The solution set must not contain duplicate subsets. Return the solution in any 
 
 Example 1:
 
-Input: nums = [1,2,3]
+Input: nums = [1,23]
 Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
  */
 fun main() {
-    val arr = intArrayOf(1,2,3)
+    val arr = intArrayOf(1,2,2)
     println(subsets(arr))
+    println(subsetsWithDup(arr))
 }
 
 /**
@@ -34,6 +35,28 @@ fun subsets(nums: IntArray): List<List<Int>> {
     fun backtrack(index: Int) {
         result.add(ArrayList(currentSubset))
         for (i in index until nums.size) {
+
+
+            currentSubset.add(nums[i])
+            backtrack(i + 1)
+            currentSubset.removeAt(currentSubset.size - 1)
+        }
+    }
+    backtrack(0)
+    return result
+}
+
+fun subsetsWithDup(nums: IntArray): List<List<Int>> {
+    val currentSubset = mutableListOf<Int>()
+    val result = mutableListOf<List<Int>>()
+
+    fun backtrack(index: Int) {
+        result.add(ArrayList(currentSubset))
+        for (i in index until nums.size) {
+            if (i > index && nums[i] == nums[i - 1]) {
+                continue // Skip this element to avoid duplicate subsets
+            }
+
             currentSubset.add(nums[i])
             backtrack(i + 1)
             currentSubset.removeAt(currentSubset.size - 1)
