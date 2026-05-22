efc499511a7393fe70a1feb0e85a29d45ba42edb
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2026-05-22 12:26:08 +0530

    Make function private

--- src/programs/arrays/SinglePassRotatedSortedSearch.kt
+++ src/programs/arrays/SinglePassRotatedSortedSearch.kt
@@ -46,7 +46,7 @@ fun main() {
 /**
  * Searches for a target value in a rotated sorted array in O(log n) time.
  */
-fun search(nums: IntArray, target: Int): Int {
+private fun search(nums: IntArray, target: Int): Int {
     var low = 0
     var high = nums.size - 1
 
